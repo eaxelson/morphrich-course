a26c7657df9842389cd486b6440552f3d3535bcc
eaxelson <erik.axelson@helsinki.fi>
2019-05-14 17:01:03 +0300

    Update lecture. Add clock and date lexc files.

--- Lecture3/src/Part1.py
+++ Lecture3/src/Part1.py
@@ -1,8 +1,10 @@
 # # MORPHOLOGICALLY RICH LANGUAGES WITH HFST TOOLS - LECTURE 3
 #
-# ## Simple lexc (numerals, dates, clocks)
+# Topics: simple lexc (numerals, dates, clocks)
 #
 # Get acquainted with <a href="https://victorio.uit.no/langtech/trunk/langs/olo/src/transcriptions/">Olonets-Karelian</a>.
+#
+# ## Numerals
 #
 # Download transcriptor-numbers-digit2text.lexc (or use a copy available in this directory).
 #
@@ -12,8 +14,48 @@ tr = compile_lexc_file('transcriptor-numbers-digit2text.lexc')
 
 # test cases
 
-tr.lookup('111')
-tr.lookup('111.')
-tr.lookup('345678')
-tr.lookup('345678.')
+print(tr.lookup('111'))
+print(tr.lookup('111.'))
+print(tr.lookup('345678'))
+print(tr.lookup('345678.'))
 
+# Note that Uralic numerals follow a pattern different from e.g. Germanic ones
+# In both language groups, numerals 1-10 must be listed individually.
+
+# In Uralic: 11-19 follow the pattern N + TOISTA (TOISTA: 'of a second')
+# In Germanic: 11, 12, 13 separate and 14-19 N + TEEN (but e.g. five -> fifteen)
+
+# In Uralic: 20-99 follow the pattern N + KYMMENTA (+ M)
+# In Germanic: same pattern but 20, 30 often separate (two -> twenty, three -> thirty || zwei/zwo -> zwanzig || två -> tjugo)
+
+# Also note that Uralic cardinals use singular partitive instead of (teen/zehn/ton || ty/ig/ti)
+# E.g. kolmesataaneljäkymmentä, 'three of a hundred four of a ten'
+
+# Ordinal numbers:
+# In Uralic: ordinality is visible in all parts: 145. -> sada*s*neljä*s*kymmene*s*viide*s*
+# In Germanic: ordinality is visible only in last part: 145. -> hundred fourty-fifth
+
+# In Uralic: cardinals and ordinals are inflected in case and in number (singular, plural, even dual?)
+
+# 145. sadasneljäskymmenesviides
+# 145. in translative: sadanneksineljänneksikymmenenneksiviidenneksi
+
+# 145. in plural: sadannetneljännetkymmenennetviidennet
+# 145. in plural translative: sadansiksineljänsiksikymmenensiksiviidensiksi
+
+# ## Dates
+#
+# https://victorio.uit.no/langtech/trunk/langs/olo/src/transcriptions/transcriptor-date-digit2text.lexc
+#
+
+tr = compile_lexc_file('transcriptor-date-digit2text.lexc')
+print(tr.lookup('1.1.'))
+
+# ## Clocks
+#
+# https://victorio.uit.no/langtech/trunk/langs/olo/src/transcriptions/transcriptor-clock-digit2text.lexc
+#
+
+tr = compile_lexc_file('transcriptor-clock-digit2text.lexc')
+print(tr.lookup('11:30'))
+print(tr.lookup('22:15'))
