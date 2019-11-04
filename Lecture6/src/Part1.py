754a4ce2cc940f982c35137306335711761c4e51
eaxelson <erik.axelson@helsinki.fi>
2019-11-04 16:35:43 +0200

    Add notes for Lecture 6.

---
+++ Lecture6/src/Part1.py
@@ -0,0 +1,38 @@
+# # MORPHOLOGICALLY RICH LANGUAGES WITH HFST TOOLS - LECTURE 6
+#
+# Topics: Shared language-independent modeling
+#
+# <ul>
+# <li>1. <a href="#1.-...">...</a></li>
+# <li>2. <a href="#2.-...">...</a></li>
+# <li>3. <a href="#3.-...">...</a></li>
+# </ul>
+
+# Finnish koivu koivujen (concatenative)
+# Finnish kala kalojen (vowel change)
+# Olonets-Karelian meččy mečän (gradation + vowel change)
+# Olonets-Karelian pata puan (gradation + diphtongisation)
+# Finnish ajatella ajattelen (two gradations)
+# Also otti-lähti, ota-lähde. Act/Pass, Tempus, Numerus, Modus (Inf/Fin) = 2 x 4 or 5 = 8 or 10 possibilities.
+
+# Long-distance dependencies can be useful in spellchecking or pedagogically.
+# Wrong kind of paradigm instead of plain typing errors.
+# E.g. auto -> audon, suo -> suoita.
+# Allow over-generation and mark wrong forms with "Err/Orth", e.g. in vro.
+
+# Stem or ending that is altered? Where should the flag/filter/trigger be placed?
+
+# E.g. Estonian +N and +A, do they follow the same pattern? With some exceptions ('nen'-endings).
+# And numerals: Finnish yhdesti, kahdesti, kolmesti, neljästi, kymmenesti, kahdestitoista, sadasti, miljoonasti?
+# Ingrian numerals.
+
+# Veps and Olonets exist, Ludian must be added.
+#
+# Diphtong to single vocal, last vowel drops. E.g. Finnish sauna -> Estonian saun.
+#
+# <table>
+# <tr><th>vep</th><th>lud</th><th>olo</th><th>en</th></tr>
+# <tr><td>hein</td><td>hein</td><td>heiny</td><td>hay</td></tr>
+# <tr><td>mar'</td><td>muar'</td><td>muarju</td><td>berry</td></tr>
+# <tr><td>ma</td><td>mua</td><td>mua</td><td>land, earth</td></tr>
+# </table>
