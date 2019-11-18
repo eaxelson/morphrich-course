8ee87fda5a31046f9253307c9b32c0868d0b17cc
eaxelson <erik.axelson@helsinki.fi>
2019-11-18 14:48:48 +0200

    Update lecture 5.

--- Lecture5/src/Part1.py
+++ Lecture5/src/Part1.py
@@ -5,7 +5,7 @@
 # <ul>
 # <li>1. <a href="#1.-Twolc-formalism">Twolc formalism</a></li>
 # <li>2. <a href="#2.-Twolc-rewrite-rules-for-Olonets-Karelian">Twolc rewrite rules for Olonets Karelian</a></li>
-# <li>3. <a href="#3.-Twolc-vs.-xfst-rewrite-rules">Twolc vs. xfst rewrite rules (TODO)</a></li>
+# <li>3. <a href="#3.-Twolc-vs.-xfst-rewrite-rules">Twolc vs. xfst rewrite rules</a></li>
 # </ul>
 
 # ## 1. Twolc formalism
@@ -14,10 +14,8 @@
 # Each part contains statements that end in `;`, possibly followed by comments that begin with `!` and span to the end of the line.
 # Two-level rules consist of a center, a rule-operator and contexts.
 # Twolc is often used for writing phonological rules that are applied to a lexicon written in lexc.
-# The rules are applied via intersection instead of composition. (TODO: explain more?)
-#
-# (It is important to mention the use and declaration of Multichar_Symbols in root.lexc
-# or wherever that happens to be.)
+# The rules are applied via <a href="https://github.com/hfst/python-hfst-4.0/wiki/HfstTransducer#compose_intersect-self-v-invertfalse">intersection</a>
+# instead of <a href="https://github.com/hfst/python-hfst-4.0/wiki/HfstTransducer#compose-self-another">composition</a>.
 #
 # A minimalistic example of lexc combined with twolc:
 #
@@ -27,8 +25,10 @@
 # kaNpa # ;
 # ```
 #
-# twolc script (available in file <i>n2m.twolc</i>) that rewrites (is this the correct term?)
-# `N` to `m` before `p`:
+# (Note that there are no multicharacter symbols in the lexc file.
+# If there were, they would have to be declared with `Multichar_Symbols`.)
+#
+# twolc script (available in file <i>n2m.twolc</i>) that rewrites # `N` to `m` before `p`:
 # ```
 # Alphabet a k m n p N ;
 # Rules
@@ -51,8 +51,21 @@ N:m <=> _ :p ;
 """
 rules = compile_twolc_script(script,verbose=True)
 
-lexicon.compose_intersect(rules)
-print(lexicon.lookup('kaNpa'))
+# The result may contain one or more rules. In this simple case, there is only one rule.
+print(rules[0])
+
+lexicon1 = lexicon.copy()
+lexicon1.compose_intersect(rules)
+print(lexicon1.lookup('kaNpa'))
+
+# Plain composition would work too, as there is only one rule.
+lexicon2 = lexicon.copy()
+lexicon2.compose(rules[0])
+print(lexicon2.lookup('kaNpa'))
+
+print(lexicon1.compare(lexicon2))
+
+exit(0)
 
 # The output is (('kampa', 0.0),).
 #
@@ -97,13 +110,13 @@ print(kala.lookup('kala+N+Pl+Ade'))
 
 # Without the rules, the result is (('kala{back}^A2O>i>l', 0.0),)
 
-# Compare with Finnish, where, instead of having a trigger to cause a:o,
+# Compare with Finnish, where, instead of having a trigger to cause `a:o`,
 # there are three different I-triggers addressing plural, past tense and the conditional
 # - i-trigger also changes to i or j.
-# In Olonets-Karelian the ^A2O trigger changes to zero and the i:j alteration
+# In Olonets-Karelian the `^A2O` trigger changes to zero and the `i:j` alteration
 # is dealt with as a separate phenomenon. Separate phenomena are easier to clean up (maybe).
-
-# In Vro:
+#
+# An example of Võro language:
 #
 # ```
 # "o2õ"
@@ -113,6 +126,7 @@ print(kala.lookup('kala+N+Pl+Ade'))
 # !!€ jõ00%>i
 # ```
 
+# Now back to Olonets-Karelian.
 # We introduce phonological rewrite rules given in file <i>olo-phon.twolc</i>. Note e.g.:
 #
 # ```
@@ -128,15 +142,13 @@ rules = compile_twolc_file('olo-phon.twolc',verbose=True)
 # We get 86 rules in total:
 print(len(rules))
 
-# (The multichar symbol %^A2O might be mentioned or underlined here.)
-# Triggers can be used for phenomena to the left and the right.
-# Be sure to sort out which direction each trigger points to,
-# e.g. in Finnish and Olonets-Karelian vowel harmony is triggered by %{back%}
-# and %{front%}, and the things affected lie to the right.
-# Consonant gradation, however, happens on the left.
-
-# Consonant fradation, as we know, happens not only in the final coda of the stem
-# but in the penultimate coda, as well:
+# At this point, a word about the multicharacter symbol `%^A2O` and triggers in general:
+#
+# * Triggers can be used for phenomena to the left and the right.
+# * Be sure to sort out which direction each trigger points to, e.g. in Finnish and Olonets-Karelian vowel harmony is triggered by `%{back%}` and `%{front%}`, and the things affected lie to the right.
+# * Consonant gradation, however, happens on the left.
+# * Consonant gradation, as we know, happens not only in the final coda of the stem but in the penultimate coda, as well, such as:
+#
 # ```
 # meččy:mečän
 # Agjatoi:agjattoma ‘loputon’
@@ -144,7 +156,8 @@ print(len(rules))
 # bringahutella+V:bringahuttel V_KUVITELLA  "heilutella" ;
 # ```
 
-# We compose-intersect the lexicon with the rules (TODO: explain what compose-intersection does)
+# We <a href="https://github.com/hfst/python-hfst-4.0/wiki/HfstTransducer#compose_intersect-self-v-invertfalse">compose-intersect</a>
+# the lexicon with the rules.
 
 kala.compose_intersect(rules)
 print(kala.lookup('kala+N+Pl+Ade'))
@@ -164,9 +177,8 @@ for testcase in ('kala+N+Pl+Ade','kala+N+Pl+Abl'):
         print(res[0].replace('>',''))
 
 # Get rid of morpheme boundary '>' so we don't have to replace it every time.
-
-# Why do we use boundaries in the first place? Boundary marker helps writing rules when we know where
-# one morpheme end and the next begins.
+# Boundary markers help writing rules when we know where one morpheme ends and the next begins,
+# but they can be filtered out at this point.
 
 from hfst_dev import EPSILON
 kala.substitute('>',EPSILON)
@@ -201,17 +213,22 @@ print(kala.lookup('kaloil'))
 
 # Test the same with koivu, 'birch'.
 
+pass # write your solution here
+
+
 # ## 3. Twolc vs. xfst rewrite rules
 #
-# TODO: Explain the differences between twolc and xfst formalism.
+# Differences between twolc and xfst formalism:
 #
-# * One thing is ordering.
-# * HFST has upkeep.
-# * Multichar-symbols in different places.
+# * Ordering.
+# * XFST has upkeep.
+# * Multichar-symbols are declared differently.
+
+# ## 4. Unexpected morphology:
 #
-# Unexpected morphology:
+# * Finnish 'joulumpi' ('Xmaser')
+# * English 'fun, funner, funnest' ('it is fun' -> 'fun' looks like adjective)
+# * English 'quickly, quicklier', and 'slowly, slowlier'
+# * "German bringen is a strong verb, and it conjugates almost just like in English" -> 'bring, brang, brung'?
 #
-# * Fin 'joulumpi' ('Xmaser')
-# * Eng fun, funner, funnest ('it is fun' -> 'fun' looks like adjective)
-# * Quick: quickly, quicklier, slowlier
-# * "German bringen is a strong verb, and it conjugates almost just like in English" -> bring: brang: brung?
+# Descriptive or prescriptive morphology?
